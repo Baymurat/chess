14cfaab1321aceeb5999704d078ea9c0e81b8c60
Baymurat <baymurat.orazov@mail.ru>
2023-01-13 18:40:25 +0500

    feat: init redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import "../style/style.scss";
+import { Provider } from "react-redux";
 import Board from "./compoentns/board";
+import { store } from "./redux/store";
+
+import "../style/style.scss";
 
 const App = () => (   
-  <div id="board-container"> 
-    <Board />
-  </div>
+  <Provider store={store}>
+    <div id="board-container"> 
+      <Board />
+    </div>
+  </Provider>
 );
 
 export default App;
