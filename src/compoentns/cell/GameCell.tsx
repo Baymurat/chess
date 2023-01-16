54536d75ccd8a242cba64898c0c533840c9a5adc
Baymurat <baymurat.orazov@mail.ru>
2023-01-16 10:43:41 +0500

    feat: add color indication of under attacked piece

--- src/compoentns/cell/GameCell.tsx
+++ src/compoentns/cell/GameCell.tsx
@@ -5,19 +5,22 @@ import { onClickCell } from "../../redux/features/board/boardSlice";
 import { useDispatch } from "react-redux";
 import { Cell } from "../../types/types";
 
-type Props = PropsWithChildren & Omit<Cell, "state">;
+// type Props = PropsWithChildren & Omit<Cell, "state">;
+type Props = PropsWithChildren & Cell;
 
 const Cell = ({
-  isPossibleMove, isSelected, isWhite, index, children 
+  isPossibleMove, isSelected, isWhite, index, children, state
 }: Props) => {
   const dispatch = useDispatch();
-  
+  const isUnderAttack = state !== "empty";
+
   return (
     <div 
       className={cx(styles.cell, { 
         [styles.white]: isWhite,
         [styles.possibleMove]: isPossibleMove,
         [styles.selected]: isSelected,
+        [styles.underAttack]: isUnderAttack,
       })}
       onClick={() => dispatch(onClickCell({ index }))}
     >
