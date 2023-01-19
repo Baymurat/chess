ea470833fff4377486cf38d27292de90a89fd014
Baymurat <baymurat.orazov@mail.ru>
2023-01-19 17:14:02 +0500

    feat: implement teastable enviroment

--- src/utils/initBoard.ts
+++ src/utils/initBoard.ts
@@ -105,7 +105,66 @@ const initPawns = (board: Cell[][], color: PieceColor): Cell[][] => {
   return copy;
 };
 
+const parseCell = (row: number) => (elem: string, index: number): Cell => {
+  if (elem === "*") {
+    return createCell("empty", [row, index], (row + index + 1) % 2 === 0);
+  }
+
+  const [p, c] = elem.split("-");
+  const color: PieceColor = c === "W" ? "white" : "black";
+  
+  let name: PieceNames = PieceNames.PAWN;
+
+  if (p === "Q") {
+    name = PieceNames.QUEEN;
+  }
+  if (p === "KG") {
+    name = PieceNames.KING;
+  }
+  if (p === "KN") {
+    name = PieceNames.KNIGHT;
+  }
+  if (p === "R") {
+    name = PieceNames.ROOK;
+  }
+  if (p === "B") {
+    name = PieceNames.BISHOP;
+  }
+
+  const piece: Piece = buildPiece(name, color);
+  const cell: Cell = createCell(piece, [row, index], (row + index + 1) % 2 === 0);
+
+  return cell;
+};
+
+const generateTestBoard = (): Cell[][] => {
+  const string_row1 = JSON.parse(process.env.ROW_1 || "");
+  const string_row2 = JSON.parse(process.env.ROW_2 || "");
+  const string_row3 = JSON.parse(process.env.ROW_3 || "");
+  const string_row4 = JSON.parse(process.env.ROW_4 || "");
+  const string_row5 = JSON.parse(process.env.ROW_5 || "");
+  const string_row6 = JSON.parse(process.env.ROW_6 || "");
+  const string_row7 = JSON.parse(process.env.ROW_7 || "");
+  const string_row8 = JSON.parse(process.env.ROW_8 || "");
+
+  const row1: Cell[] = string_row1.map(parseCell(0));
+  const row2: Cell[] = string_row2.map(parseCell(1));
+  const row3: Cell[] = string_row3.map(parseCell(2));
+  const row4: Cell[] = string_row4.map(parseCell(3));
+  const row5: Cell[] = string_row5.map(parseCell(4));
+  const row6: Cell[] = string_row6.map(parseCell(5));
+  const row7: Cell[] = string_row7.map(parseCell(6));
+  const row8: Cell[] = string_row8.map(parseCell(7));
+
+  const board: Cell[][] = [row1, row2, row3, row4, row5, row6, row7, row8];
+  return board;
+};
+
 export const generateBoard = (): Cell[][] => {
+  if (process.env.IS_TEST === "true") {
+    return generateTestBoard();
+  }
+
   let board: Cell[][] = [];
   board = createEmptyBoard();
   board = initOfficers(board, "white");
